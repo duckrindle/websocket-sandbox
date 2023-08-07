eda2093dea99e7eb7f990cbb8aa388c6ac7ff2d2
Duck Rindle <duckrindle@protonmail.com>
2023-08-07 15:39:40 +0200

    clean server code

--- server.js
+++ server.js
@@ -1,17 +1,11 @@
 const express = require('express');
 
-console.log('1');
-
 const server = express()
   .use((req, res) => res.sendFile('/index.html', { root: __dirname }))
   .listen(3000, () => console.log(`Listening on ${3000}`));
 
-console.log('2');
-
 const { Server } = require('ws');
 const ws_server = new Server({ server });
-
-console.log('3');
 
 ws_server.on('connection', (ws) => {
   console.log('New client connected!');
@@ -19,12 +13,8 @@ ws_server.on('connection', (ws) => {
   ws.on('close', () => console.log('Client has disconnected!'));
 });
 
-console.log('4');
-
 setInterval(() => {
   ws_server.clients.forEach((client) => {
     client.send(new Date().toTimeString());
   });
 }, 1000);
-
-console.log('5');
